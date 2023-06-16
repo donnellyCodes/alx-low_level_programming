f0fdd31b082ead73bf83eda636695d67957fdafe
Donnelly <donamaitsa@gmail.com>
2023-06-15 17:42:48 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - 4-puts.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-}	
+}
