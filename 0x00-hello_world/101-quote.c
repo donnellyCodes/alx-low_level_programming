49d63f27bab28475f30a6417603fca5e32afdef4
Donnelly <donamaitsa@gmail.com>
2023-06-15 18:46:40 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - 101-quote.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (0);
+}
