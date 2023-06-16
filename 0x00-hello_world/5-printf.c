b2c2b18f2409138624efa24bed1f0274df9f9e96
Donnelly <donamaitsa@gmail.com>
2023-06-15 17:58:16 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - 5-printf.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
