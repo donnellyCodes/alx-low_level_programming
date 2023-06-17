5e9693486ca8f89f1df34f46733c789c3e5bcc51
Donnelly <donamaitsa@gmail.com>
2023-06-16 21:11:53 -0700

    Task 12.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - a program that prints all possible combinations
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	int y;
+
+	for (x = 0; x <= 98; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 98 && y == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
