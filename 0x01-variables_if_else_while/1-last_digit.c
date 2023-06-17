16ee4985d4f5e9aaef1eb6706b5e0346f4e2733c
Donnelly <donamaitsa@gmail.com>
2023-06-16 18:53:55 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less that 6 and not 0\n", n, ld);
+		printf("Last digit of %d is %d and less that 6 and not 0\n", n, ld);
 	}
 	return (0);
 }
