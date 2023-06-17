c7570d345be01e00fda7a1c01faa2ebb659ccd46
Donnelly <donamaitsa@gmail.com>
2023-06-16 17:44:04 -0700

    Task 7.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - program that prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
