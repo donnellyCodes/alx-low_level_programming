8c9ef41854edb0b2bc3b1c8d6553cb0860395d9f
Donnelly <donamaitsa@gmail.com>
2023-06-16 17:32:51 -0700

    Task 6.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - a program that prints all single digit numbers
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10) {
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+
+}
+
