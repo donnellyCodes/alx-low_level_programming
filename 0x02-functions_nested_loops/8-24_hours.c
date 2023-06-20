febe92f923fe8dbd1d083444b5ab04505a7aa364
Donnelly <donamaitsa@gmail.com>
2023-06-20 15:23:18 -0700

    Task 8. C functions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - function to print every minute of a day
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	int j;
+
+	i = 0;
+	while (1 < 24)
+	{
+		j  = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
