38129de5091d4cf471b4ad0b8185e9b6535f1e4d
Donnelly <donamaitsa@gmail.com>
2023-06-20 13:37:15 -0700

    Task 2. C functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - 2-alphabet_x10
- * description - a function that prints 10 times the alphabet
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * followed by a new line
  *
  * Return: Always 0
  */
