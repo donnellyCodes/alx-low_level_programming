0fad9607a68ab44a96d18c8f49666e48f3fa15d6
Donnelly <donamaitsa@gmail.com>
2023-06-20 17:03:33 -0700

    Task 10. C function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - function used to add two variables
+ * @a:first integer
+ * @b:second integer
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
