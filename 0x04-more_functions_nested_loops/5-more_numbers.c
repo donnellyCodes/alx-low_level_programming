b19e3da0126d41b33217b2b43c29c44b24dfe3ac
Donnelly <donamaitsa@gmail.com>
2023-06-22 05:52:35 -0700

    Task 5. C functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints numbers upto 14
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
