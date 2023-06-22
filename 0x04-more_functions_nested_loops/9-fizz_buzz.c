1bbd3880ce565a53aae0c4ed1413ffcd80bf2ea6
Donnelly <donamaitsa@gmail.com>
2023-06-22 07:59:46 -0700

    Task 9. C more functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,32 +7,19 @@ int main(void)
 {
 	int num;
 
-	while (num++ < 100)
+	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
-		}
-		else
-		{
+		if ((num % 3 == 0) && (num % 5 != 0))
+			printf(" Fizz");
+		else if ((num % 5) == 0 && (num % 3 != 0))
+			printf(" Buzz");
+		else if ((num % 3 == 0) && (num % 5 == 0))
+			printf(" FizzBuzz");
+		else if (num == 1)
 			printf("%d", num);
-		}
-		printf("\n");
+		else
+			printf(" %d", num);
 	}
-		return (0);
+	printf("\n");
+	return (0);
 }
