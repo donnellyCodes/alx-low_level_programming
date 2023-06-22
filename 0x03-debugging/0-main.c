d36cd6256f91eda0bec40be00c37f298a29dfadc
Donnelly <donamaitsa@gmail.com>
2023-06-22 11:36:05 -0700

    Task 0. C debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - function that prints positive and negative integers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
