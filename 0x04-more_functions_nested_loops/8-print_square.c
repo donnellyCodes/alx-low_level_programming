9df8b4f5ae594665a39a25c37acc032fc9897082
Donnelly <donamaitsa@gmail.com>
2023-06-22 07:04:39 -0700

    Task 7. C more functions

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square
+ * @size: parameter
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc1 = 0; inc1 < size; inc1++)
+		{
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			{
+				_putchar('#');
+			}
+
+				_putchar('#');
+				_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
