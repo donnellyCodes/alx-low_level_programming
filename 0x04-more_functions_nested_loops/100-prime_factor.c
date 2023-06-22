b3775b8e3ee840f2fb7500074be18e9f9e37e829
Donnelly <donamaitsa@gmail.com>
2023-06-22 14:26:12 -0700

    Task 11. C more functions

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - program finds and prints largest prime number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long number = 612852475143;
+
+	int inc;
+
+	while (inc++ < number / 2)
+	{
+		if (number % inc == 0)
+		{
+			number /= 2;
+			continue;
+		}
+		for (inc = 3; inc < number / 2; inc += 2)
+		{
+			if (number % inc == 0)
+				number /= inc;
+		}
+	}
+	printf("%ld\n", number);
+	return (0);
+}
