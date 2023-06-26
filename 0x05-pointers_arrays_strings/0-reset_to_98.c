3fe231a851b92629f1daf4ddc0cd135e4088f664
Donnelly <donamaitsa@gmail.com>
2023-06-26 07:55:14 -0700

    Task 0. C pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * @n: pointer to the variable to be updated
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
