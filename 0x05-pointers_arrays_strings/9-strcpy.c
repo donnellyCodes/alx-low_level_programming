459cde81aaac1095b7c3ae5f976a583bbca6f653
Donnelly <donamaitsa@gmail.com>
2023-06-26 12:11:09 -0700

    Task 9. C pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy -  Copy and paste string
+ * @dest: destination
+ * @src: source
+ * Return: Always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
