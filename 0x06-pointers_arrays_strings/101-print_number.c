19f787293ac38bcce407eabf6d9e7d50fb0363d2
Donnelly <donamaitsa@gmail.com>
2023-06-29 07:54:33 -0700

    Task 9. C pointers and arrays

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,19 +7,13 @@
 int _putchar(char c);
 void print_number(int n)
 {
-	unsigned int n1;
-
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
+		n = -n;
 	}
-	else
-	{
-		n1 = n;
-	}
-	if (n1 / 10)
-		print_number(n1 / 10);
+	if (n / 10)
+		print_number(n / 10);
 
-	_putchar((n1 % 10) + '0');
+	_putchar((n % 10) + '0');
 }
