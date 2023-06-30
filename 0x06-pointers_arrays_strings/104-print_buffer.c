69c7e36b73c4788e099048e87dc9111c12f41034
Donnelly <donamaitsa@gmail.com>
2023-06-30 15:29:30 -0700

    Task 12. C pointers and array

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,41 +1,50 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_buffer - function that prints a buffer
- * @size: size of byte
- * @b: byte
- * Return: Always 0
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int i;
+	int o, j, i;
 
-	int j;
+	o = 0;
 
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x", i);
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-				printf("%02x", b[i + j]);
-			else
-				printf(" ");
-		}
-		printf(" ");
-		for (j = 0; j < 10 && i + j < size; j++)
-		{
-			char c = b[i + j];
-
-			if (c >= 32 && c < 127)
-				printf("%c", c);
-			else
-				printf(".");
-		}
-		printf("\n");
-	}
 	if (size <= 0)
 	{
 		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
 }
