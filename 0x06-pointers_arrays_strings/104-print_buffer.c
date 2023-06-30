fb6173c3dee4367a6d39315cf1674deeea095321
Donnelly <donamaitsa@gmail.com>
2023-06-30 00:37:46 -0700

    Task 12. C pointers and array

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,26 +14,28 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%08x: ", i);
+		printf("%08x", i);
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
 				printf("%02x", b[i + j]);
 			else
 				printf(" ");
-			if (j % 2 == 1)
-				printf(" ");
 		}
-		for (j = 0; j < 10; j++)
+		printf(" ");
+		for (j = 0; j < 10 && i + j < size; j++)
 		{
-			if (i + j < size)
-			{
-				if (b[i + j] >= ' ' && b[i + j] <= '~')
-					printf("%c", b[i + j]);
-				else
-					printf(".");
-			}
+			char c = b[i + j];
+
+			if (c >= 32 && c < 127)
+				printf("%c", c);
+			else
+				printf(".");
 		}
+		printf("\n");
+	}
+	if (size <= 0)
+	{
 		printf("\n");
 	}
 }
