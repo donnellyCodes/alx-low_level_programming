5fb967249a2690324806c2dcd5bf6e1c66b5f04d
Donnelly <donamaitsa@gmail.com>
2023-07-03 05:41:18 -0700

    Task 2, strchr

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c);
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
+}
