8040fddb8becbae7f30ca731b00e6d16e57d31da
Donnelly <donamaitsa@gmail.com>
2023-07-05 09:00:54 -0700

    Recursion comparing two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * wildcmp - functio to compare strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 1 if the strings are identical, 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+		{
+			return (1);
+		}
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
