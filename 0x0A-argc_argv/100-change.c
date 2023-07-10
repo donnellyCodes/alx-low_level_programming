bb04d1cf0b9502a765cff75a312ca4970c59b427
Donnelly <donamaitsa@gmail.com>
2023-07-10 02:37:36 -0700

    Printing minimum number of coins using argc and argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,52 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
- * @argc: input
- * @argv: input
+ * main - prints minimum number of coins for an amount of money
+ * @argc: count two arguments
+ * @argv: arguments given should be program name and amount of money
  * Return: Always 0 (success)
 **/
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int n, coins = 0;
 
-	if (argc == 2)
-	{
-		cents = atoi(*(argv + 1));
-		while (cents > 0)
-		{
-			if (cents % 25 < cents)
-			{
-				cents -= 25;
-				coins++;
-			}
-			else if (cents % 10 < cents)
-			{
-				cents -= 10;
-				coins++;
-			}
-			else if (cents % 5 < cents)
-			{
-				cents -= 5;
-				coins++;
-			}
-			else if (cents % 2 < cents)
-			{
-				cents -= 2;
-				coins++;
-			}
-			else if (cents % 1 < cents)
-			{
-				cents -= 1;
-				coins++;
-			}
-		}
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	if (argv[1][0] == '-')
+	{
+		printf("0\n");
+		return (0);
+	}
+	n = atoi(argv[1]);
+
+	coins += n / 25;
+	n = n % 25;
+	coins += n / 10;
+	n = n % 10;
+	coins += n / 5;
+	n = n % 5;
+	coins += n / 2;
+	n  = n % 2;
+	coins += n / 1;
+
 	printf("%d\n", coins);
 	return (0);
 }
